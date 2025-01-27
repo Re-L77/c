6710fa3665875215c8492efcc34730907ce280ef
Knives77 <pepinilloinquisidor@gmail.com>
2025-01-27 00:04:25 -0600

    1.1

--- Proyects/OrdenarNumerosEnteros/main.c
+++ Proyects/OrdenarNumerosEnteros/main.c
@@ -3,37 +3,37 @@
 #include <locale.h>
 
 /*
-	Programa que ordena 3 nÃºmeros enteros diferentes y verifica en caso de ser
+	Programa que ordena 3 números enteros diferentes y verifica en caso de ser
 	iguales.
 	Autor: Ordaz Magos Juan Pablo
-	Fecha de creaciÃ³n: 20/01/25
+	Fecha de creación: 20/01/25
 */
 int primerNumero, segundoNumero, tercerNumero;
 int main(int argc, char *argv[])
 {
 	setlocale(LC_ALL, "");
-	printf("Introduce el primer nÃºmero: ");
+	printf("Introduce el primer número: ");
 	scanf("%d", &primerNumero);
-	printf("Introduce el segundo nÃºmero: ");
+	printf("Introduce el segundo número: ");
 	scanf("%d", &segundoNumero);
-	printf("Introduce el tercer nÃºmero: ");
+	printf("Introduce el tercer número: ");
 	scanf("%d", &tercerNumero);
 
-	// ValidaciÃ³n en caso de los nÃºmeros sean iguales.
+	// Validación en caso de los números sean iguales.
 	if (primerNumero == segundoNumero && segundoNumero == tercerNumero)
 	{
-		printf("Los 3 nÃºmeros son iguales.\n");
+		printf("Los 3 números son iguales.\n");
 	}
 	else if (primerNumero == segundoNumero)
 	{
 		if (primerNumero > tercerNumero)
 		{
-			printf("El primer y segundo nÃºmero son iguales y mayores que el tercer nÃºmero.\n");
+			printf("El primer y segundo número son iguales y mayores que el tercer número\n");
 			printf("%d, %d > %d\n", primerNumero, segundoNumero, tercerNumero);
 		}
 		else
 		{
-			printf("El tercer nÃºmero es mayor que el primer y segundo nÃºmero.\n");
+			printf("El tercer número es mayor que el primer y segundo número\n");
 			printf("%d > %d, %d\n", tercerNumero, primerNumero, segundoNumero);
 		}
 	}
@@ -41,12 +41,12 @@ int main(int argc, char *argv[])
 	{
 		if (segundoNumero > primerNumero)
 		{
-			printf("El segundo y tercer nÃºmero son iguales y mayores que el primer nÃºmero.\n");
+			printf("El segundo y tercer número son iguales y mayores que el primer número\n");
 			printf("%d < %d, %d\n", primerNumero, tercerNumero, segundoNumero);
 		}
 		else
 		{
-			printf("El primer nÃºmero es mayor que el segundo y tercer nÃºmero.\n");
+			printf("El primer número es mayor que el segundo y tercer número\n");
 			printf("%d > %d, %d\n", primerNumero, segundoNumero, tercerNumero);
 		}
 	}
@@ -54,19 +54,19 @@ int main(int argc, char *argv[])
 	{
 		if (primerNumero > segundoNumero)
 		{
-			printf("El primer y tercer nÃºmero son iguales y mayores que el segundo nÃºmero.\n");
+			printf("El primer y tercer número son iguales y mayores que el segundo número.\n");
 			printf("%d > %d < %d\n", primerNumero, segundoNumero, tercerNumero);
 		}
 		else
 		{
-			printf("El segundo nÃºmero es mayor que el primer y tercer nÃºmero.\n");
+			printf("El segundo número es mayor que el primer y tercer número.\n");
 			printf("%d < %d > %d\n", primerNumero, segundoNumero, tercerNumero);
 		}
 	}
-	// Una vez verificado que los nÃºmeros son diferentes, se procede a ordenarlos.
+	// Una vez verificado que los números son diferentes, se procede a ordenarlos.
 	else if (primerNumero > segundoNumero && primerNumero > tercerNumero)
 	{
-		printf("El primer nÃºmero es el mayor.\n");
+		printf("El primer número es el mayor.\n");
 		if (segundoNumero > tercerNumero)
 		{
 			printf("%d, %d, %d\n", primerNumero, segundoNumero, tercerNumero);
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
 	{
 		if (segundoNumero > primerNumero && segundoNumero > tercerNumero)
 		{
-			printf("El segundo nÃºmero es el mayor.\n");
+			printf("El segundo número es el mayor.\n");
 			if (primerNumero > tercerNumero)
 			{
 				printf("%d, %d, %d\n", segundoNumero, primerNumero, tercerNumero);
@@ -92,7 +92,7 @@ int main(int argc, char *argv[])
 		}
 		else
 		{
-			printf("El tercer nÃºmero es el mayor.\n");
+			printf("El tercer número es el mayor.\n");
 			if (primerNumero > segundoNumero)
 			{
 				printf("%d, %d, %d\n", tercerNumero, primerNumero, segundoNumero);
