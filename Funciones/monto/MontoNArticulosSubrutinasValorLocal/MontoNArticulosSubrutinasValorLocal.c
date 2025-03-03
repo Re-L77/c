e7bf8a900ad1a3d7df93bd9bf0006f2f579de66c
RE-L77 <124051537@upq.edu.mx>
2025-03-03 08:36:14 -0600

    4.0

--- Funciones/monto/MontoNArticulosSubrutinasValorLocal/MontoNArticulosSubrutinasValorLocal.c
+++ Funciones/monto/MontoNArticulosSubrutinasValorLocal/MontoNArticulosSubrutinasValorLocal.c
@@ -1,20 +1,17 @@
-#include "stdio.h"
-#include "stdlib.h"
 #include "locale.h"
 #include "stdbool.h"
+#include "stdio.h"
+#include "stdlib.h"
 /*
-* Programa que calcula el monto de n artículos con uso de subrutinas y valores locales:.
-* Autor: Ordaz Magos Juan Pablo
-* Fecha: 25-02-25
-* Versión: 3.0
-*/
+ * Programa que calcula el monto de n artículos con uso de subrutinas y valores
+ * locales:. Autor: Ordaz Magos Juan Pablo Fecha: 25-02-25 Versión: 3.0
+ */
 
 int leerArticulos();
 float calcularMonto(float articulos);
 void mostrarMonto(float monto);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   setlocale(LC_ALL, "");
   float total;
   char res;
@@ -25,35 +22,35 @@ int main(int argc, char *argv[])
     printf("\n¿Desear calcular de otro monto? ");
     fflush(stdin);
     scanf(" %c", &res);
-  }while (res == 's' || res == 'S');
-   return EXIT_SUCCESS;
+  } while (res == 's' || res == 'S');
+  return EXIT_SUCCESS;
 }
 
-int leerArticulos(){
+int leerArticulos() {
   static int articulos;
   do {
     printf("Ingresa la cantidad de artículos: ");
-    scanf("%i",&articulos);
+    scanf("%i", &articulos);
     if (articulos <= 0) {
       printf("Error debe ser mayor a 0.\n");
     }
-  }while (articulos <= 0 );
+  } while (articulos <= 0);
   return articulos;
 }
 
-float leerPrecio(){
-  static float precio; 
+float leerPrecio() {
+  static float precio;
   do {
     printf("Precio del artículo:");
     scanf("%f", &precio);
     if (precio <= 0) {
       printf("Error debe ser mayor a 0.\n");
     }
-  }while (precio <= 0);
+  } while (precio <= 0);
   return precio;
 }
 
-float calcularMonto(float articulos){
+float calcularMonto(float articulos) {
   static int contador;
   static float monto;
   monto = 0;
@@ -65,6 +62,4 @@ float calcularMonto(float articulos){
   return monto;
 }
 
-void mostrarMonto(float monto) {
-  printf("Monto total: %.2f\n", monto);
-}
+void mostrarMonto(float monto) { printf("Monto total: %.2f\n", monto); }
