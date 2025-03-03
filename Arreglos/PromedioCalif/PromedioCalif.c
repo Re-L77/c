1c18c534d2d8b9540189aa23c1ac8808f6532fcd
RE-L77 <124051537@upq.edu.mx>
2025-03-02 21:21:45 -0600

    4.0

--- Arreglos/PromedioCalif/PromedioCalif.c
+++ Arreglos/PromedioCalif/PromedioCalif.c
@@ -1,60 +1,78 @@
-#include "stdio.h"
-#include "stdlib.h"
 #include "locale.h"
 #include "stdbool.h"
+#include "stdio.h"
+#include "stdlib.h"
 
 /*
- * Este programa ejemplifica el uso de funciones y procedimientos con valores locales
- * Autor: Ordaz Magos Juan Pablo
- * Fecha: 19-02-2025
-*/
+ * Este programa ejemplifica el uso de funciones y procedimientos con valores
+ * locales y arreglos Autor: Ordaz Magos Juan Pablo Fecha: 26-02-2025 v: 3.0
+ */
 
-//Declaración de procedimientos y funciones
-float validaCalif(float calif){
-  while (calif < 0 || calif > 10) {
-    printf("\n La calificación digita es erronea");
-    printf("\n Solo se aceptan valores entre 0 y 10");
-    printf("\n Digita nuevamente la calificación: ");
-    scanf("%f", &calif);
-  }
-  return calif;
-}
+void capturaCalif();
+void validaCalif(int CuentaCalif, float calificacion);
+void verDatos();
+float calcular();
+void verProm(float promedio);
 
-float pedirCalif() {
-  //Variables locales
-  int CuentaCalif;
-  float calif, suma, califValida;
-  suma=0;
-  for (CuentaCalif = 1; CuentaCalif <= 3; CuentaCalif++) {
-    printf("\nDigita la calificaión %i del alumno: ", CuentaCalif);
-    scanf("%f", &calif);
-    califValida = validaCalif(calif);
-    suma += califValida;
-  }
-  return suma;
-}
-
-float calcularProm(float suma) {
-  return suma / 3;
-}
-
-void mostrarRes(float promedio) {
-  printf("\nEl promedio del alumno es: %.2f\n", promedio);
-}
+float calificaciones[3];
+float calificacion;
 
 int main(int argc, char *argv[]) {
   setlocale(LC_ALL, "");
   char resp;
   float acumulador, promedio;
-
   do {
     system("clear");
-    acumulador = pedirCalif();
-    promedio = calcularProm(acumulador);
-    mostrarRes(promedio);
-    printf("¿Deseas calcular otro promedio?\n");
+    capturaCalif();
+    promedio = calcular();
+    verDatos();
+    verProm(promedio);
+    printf("\n¿Deseas calcular otro promedio?: ");
     fflush(stdin);
     scanf(" %c", &resp);
   } while (resp == 's' || resp == 'S');
   return EXIT_SUCCESS;
+}
+
+void capturaCalif() {
+  int CuentaCalif;
+  for (CuentaCalif = 0; CuentaCalif <= 2; CuentaCalif++) {
+    printf("\nCalificación [%i]: ", CuentaCalif);
+    scanf("%f", &calificacion);
+    calificaciones[CuentaCalif] = calificacion;
+    validaCalif(CuentaCalif, calificaciones[CuentaCalif]);
+  }
+}
+
+// Declaración de procedimientos y funciones
+void validaCalif(int CuentaCalif, float calificacion) {
+  while (calificacion < 0 || calificacion > 10) {
+    printf("\n La calificación digita es erronea");
+    printf("\n Solo se aceptan valores entre 0 y 10");
+    printf("\n Digita nuevamente la calificación: ");
+    scanf("%f", &calificacion);
+    calificaciones[CuentaCalif] = calificacion;
+  }
+}
+
+void verDatos() {
+  int i;
+  printf("\n");
+  for (i = 0; i <= 2; i++) {
+    printf("[%.2f] ", calificaciones[i]);
+  }
+}
+
+void verProm(float promedio) { printf("\nPromedio: %.2f", promedio); }
+
+float calcular() {
+  static float suma;
+  static int index;
+  static float promedio;
+  suma = 0;
+  for (index = 0; index <= 2; index++) {
+    suma += calificaciones[index];
+  }
+  promedio = suma / 3;
+  return promedio;
 }
---
+++ Funciones/monto/MontoNArticulos/es.js
@@ -0,0 +1,7 @@
+art = 5;
+cont = 1;
+6<=5
+while (cont <= art) {
+  
+  cont = cont + 1;
+}
