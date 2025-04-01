5bbe3e332840db7bc49464d80778c3ada3745140
Knives77 <pepinilloinquisidor@gmail.com>
2025-03-31 20:19:53 -0600

    4.6

--- 4.0/SalarioNEmpleado/src/SalarioNEmpleado.c
+++ 4.0/SalarioNEmpleado/src/SalarioNEmpleado.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #ifdef _WIN32
 #include <conio.h>
 #endif
@@ -17,15 +18,17 @@
 
 FILE *archivo;
 int opcion, n;
-typedef struct {
+typedef struct
+{
   int id;
   char departamento[100];
   float salario;
   float ventas;
 } Empleado;
 
-int opcionMenu() {
-  printf("\n\t\tMenú\n");
+int opcionMenu()
+{
+  printf("\n\n\t\tMenú\n");
   printf("1. Cargar datos.\n");
   printf("2. Ver datos\n");
   printf("3. Salir\n");
@@ -35,114 +38,183 @@ int opcionMenu() {
   return opcion;
 }
 
-void limpiarPantalla() {
+void limpiarPantalla()
+{
 #ifdef _WIN32
   system("cls");
 #else
   system("clear");
 #endif
 }
-void pedirN() {
-  do {
+void pedirN()
+{
+  do
+  {
     printf("\nNúmero de empleados: ");
     scanf("%d", &n);
-    if (n <= 0) {
+    if (n <= 0)
+    {
       printf("\n[ERROR] - Número inválido.");
     }
   } while (n <= 0);
 }
 
-int validarID() {
+int validarID()
+{
   int id;
-  do {
+  do
+  {
     printf("\nIngrese ID del empleado: ");
     scanf("%d", &id);
-    if (id <= 0) {
+    if (id <= 0)
+    {
       printf("\n[ERROR] - Ingresa un ID válido.");
     }
   } while (id <= 0);
   return id;
 }
 
-float validarSalario() {
+float validarSalario()
+{
   float salario;
-  do {
+  do
+  {
     printf("\nIngrese salario del empleado: ");
     scanf("%f", &salario);
-    if (salario <= 0) {
+    if (salario <= 0)
+    {
       printf("\n[ERROR] - Ingresa un salario válido. (salario > 0)");
     }
   } while (salario <= 0);
   return salario;
 }
 
-float validarVentas() {
+float validarVentas()
+{
   float ventas;
-  do {
+  do
+  {
     printf("\nIngrese ventas del empleado: ");
     scanf("%f", &ventas);
-    if (ventas <= 0) {
+    if (ventas <= 0)
+    {
       printf("\n[ERROR] - Ingresa una venta válida. (venta > 0)");
     }
   } while (ventas <= 0);
   return ventas;
 }
 
-void cargarDatos() {
-  char departamento[100];
-  archivo = fopen(ARCHIVO, "r+");
-  if (archivo == NULL) {
+void cargarDatos()
+{
+  archivo = fopen(ARCHIVO, "w+");
+  if (archivo == NULL)
+  {
     printf("\n[ERROR] - al abrir el archivo");
     return;
   }
-
-  Empleado nuevoEmpleado;
-  nuevoEmpleado.id = validarID();
-  printf("\nIngresa el departamento: ");
-  scanf(" %s", &departamento[0]);
-  strcpy(nuevoEmpleado.departamento, departamento);
-  nuevoEmpleado.salario = validarSalario();
-  nuevoEmpleado.ventas = validarVentas();
-
-  /*printf("\nid: %d, dep: %s, sal: %.2f, vent: %.2f", nuevoEmpleado.id,
-         nuevoEmpleado.departamento, nuevoEmpleado.salario,
-         nuevoEmpleado.ventas);*/
-  fprintf(archivo, "%d|%s|%f|%f\n", nuevoEmpleado.id,
-          nuevoEmpleado.departamento, nuevoEmpleado.salario,
-          nuevoEmpleado.ventas);
-  printf("\n------[OK] - Agregado.------");
-  fclose(archivo);
-}
-
-void verDatos() {
-  archivo = fopen(ARCHIVO, "r");
-  if (archivo == NULL) {
-    printf("\n[ERROR] - al abrir el archivo");
-    return;
-  }
-  fclose(archivo);
-}
-
-void incrementar() {}
-int main(int argc, char *argv[]) {
-  setlocale(LC_ALL, "");
-  do {
+  int i;
+  for (i = 1; i <= n; i++)
+  {
     limpiarPantalla();
+    printf("\n\tEmpleado %d\n", i);
+    printf("--------------------------------\n");
+    Empleado nuevoEmpleado;
+    nuevoEmpleado.id = validarID();
+    printf("\nIngresa el departamento: ");
+    scanf(" %99s", nuevoEmpleado.departamento);
+    nuevoEmpleado.salario = validarSalario();
+    nuevoEmpleado.ventas = validarVentas();
+    fprintf(archivo, "%d,%s,%.2f,%.2f\n", nuevoEmpleado.id,
+            nuevoEmpleado.departamento, nuevoEmpleado.salario,
+            nuevoEmpleado.ventas);
+  }
+  printf("\n------[OK] - Agregados.------");
+  fclose(archivo);
+}
 
+void verDatos()
+{
+  archivo = fopen(ARCHIVO, "r");
+  if (archivo == NULL)
+  {
+    printf("\n[ERROR] - al abrir el archivo");
+    return;
+  }
+  printf("\n\t\tDatos de empleados\n");
+  printf("ID|Departamento|Salario|Ventas\n");
+  printf("--------------------------------\n");
+  Empleado empleado;
+  while (fscanf(archivo, "%d,%99[^,],%f,%f\n", &empleado.id,
+                empleado.departamento, &empleado.salario,
+                &empleado.ventas) != EOF)
+  {
+    printf("%d,%s,%.2f,%.2f\n", empleado.id, empleado.departamento,
+           empleado.salario, empleado.ventas);
+  }
+  printf("--------------------------------\n");
+  fclose(archivo);
+}
+
+void incrementar()
+{
+  archivo = fopen(ARCHIVO, "r");
+  if (archivo == NULL)
+  {
+    printf("\n[ERROR] - al abrir el archivo");
+    return;
+  }
+  FILE *temp = fopen("temp.txt", "w");
+  if (temp == NULL)
+  {
+    printf("\n[ERROR] - al crear archivo temporal");
+    fclose(archivo);
+    return;
+  }
+  Empleado empleado;
+  while (fscanf(archivo, "%d,%99[^,],%f,%f\n", &empleado.id,
+                empleado.departamento, &empleado.salario,
+                &empleado.ventas) != EOF)
+  {
+    if (empleado.ventas > 500000.00)
+    {
+      empleado.salario *= 1.1;
+    }
+    fprintf(temp, "%d,%s,%.2f,%.2f\n", empleado.id,
+            empleado.departamento, empleado.salario,
+            empleado.ventas);
+  }
+  fclose(archivo);
+  fclose(temp);
+  remove(ARCHIVO);
+  rename("temp.txt", ARCHIVO);
+  printf("\n------[OK] - Salarios actualizados.------\n");
+}
+
+int main(int argc, char *argv[])
+{
+  setlocale(LC_ALL, "");
+  do
+  {
     opcion = opcionMenu();
-    switch (opcion) {
+    switch (opcion)
+    {
     case 1:
+      limpiarPantalla();
       pedirN();
       cargarDatos();
+      incrementar();
       break;
     case 2:
+      limpiarPantalla();
       verDatos();
       break;
     case 3:
-      printf("\nSaliendo del programa...");
+      limpiarPantalla();
+      printf("\nSaliendo del programa...\n");
       break;
     default:
-      printf("[ERROR] - Valor no admitido");
+      limpiarPantalla();
+      printf("[ERROR] - Valor no admitido\n");
       break;
     }
   } while (opcion != 3);
